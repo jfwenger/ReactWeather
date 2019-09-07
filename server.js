b14d845f3054941a3b474f91d7c3cfffd90b10c7
John F. Wenger <john.f.genius@gmail.com>
2019-09-07 21:13:05 -0200

    Trying to Beat Server Error

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var express = require('express')
+var express = require('express');
+
+// Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
-  if (res.headers['x-forwarded-proto'] === "http") {
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
-    res.redirect("http://" + req.hostname + req.url);
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
+
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-   console.log('Express server running on port' + PORT + ".");
+  console.log('Express server is up on port ' + PORT);
 });
